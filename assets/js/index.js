ebd810a4c771edc173f5ccaa64f16a563c0f7715
Romanskiy94 <bensupermartin@gmail.com>
2019-12-06 20:49:24 +0200

    asf

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,18 +35,70 @@ fetch('./user.json')
     .then( appendUsersToList )
     .catch(console.error);
 
-
-
-
 function appendUsersToList(users) {
+
+    const userListElem = document.getElementById('usersList')
+
+    users.forEach(
+        user => {
+            userListElem.appendChild(createUserCardElem(user))
+        }
+    )
+}
+
+function createUserCardElem(user){
+
+    const userListItem = createUserListItemElem(user);
+
+    userListItem.appendChild(createUserImageElem(user));
+    userListItem.appendChild(createUserFullName(user));
+
+    return userListItem;
+}
+
+function createUserListItemElem(user){
+
+    const userListItem = document.createElement("li");
+
+    userListItem.setAttribute("id", user.id);
+    userListItem.classList.add("userListItem");
+
+    return userListItem;
+}
+
+function createUserImageElem(user) {
+    const userImage = document.createElement("img");
+
+    userImage.setAttribute("src",user.profilePictureSrc);
+    userImage.setAttribute("alt", "profile picture");
+
+    const userImageContainer = document.createElement("div");
+
+    userImageContainer.classList.add("profilePictureContainer");
+    userImageContainer.appendChild(userImage);
+
+    return userImageContainer;
 
 }
 
+function createUserFullName({name, surname}) {
 
+    const userFullNameElem = document.createElement('h3')
 
+    userFullNameElem.classList.add('userFullName');
+    userFullNameElem.innerText = `${name} ${surname}`;
 
+    return userFullNameElem;
 
+}
 
+function userNotPicture() {
+
+    if (createUserImageElem){
+
+    }
+
+}
 
 
 
